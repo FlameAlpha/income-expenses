4903012bd1e2e9286d7a2f39b59f82cfa5816acc
魏士杰 <sayitagain.flame@foxmail.com>
2020-06-20 10:01:32 +0800

    添加注释

--- income_expenses/income_expenses.cpp
+++ income_expenses/income_expenses.cpp
@@ -1,4 +1,4 @@
-﻿// income_expenses.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// income_expenses.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "pch.h"
@@ -9,7 +9,7 @@
 #include <fstream>
 #include <numeric>
 
-
+// 按 delimiter 分割字符串 
 bool splite_line(const std::string & strLine, std::vector<std::string> & stringVector, const char & delimiter = '-') {
 	unsigned long long nBegin = 0;
 	unsigned long long nEnd = 0;
@@ -31,6 +31,7 @@ bool splite_line(const std::string & strLine, std::vector<std::string> & stringV
 	return flag;
 }
 
+// 时间存储
 class Time {
 public:
 	int year;
@@ -38,18 +39,21 @@ public:
 	int date;
 };
 
+// 获取从当前位置开始的第一个非空格字符
 int get_first_character(int index, const std::string & info_str) {
 	while (index < info_str.length() && info_str[index] == ' ')
 		++index;
 	return index < info_str.length() ? index : -1;
 }
 
+// 获取从当前位置开始的最后一个非空格字符
 int get_end_character(int index, const std::string & info_str) {
 	while (index < info_str.length() && info_str[index] != ' ')
 		++index;
 	return index;
 }
 
+// 获取从当前位置开始的最后一个非空格单词 -- 连续的非空格字符串
 std::string get_first_word(int & index, const std::string & info_str) {
 	int start_index = get_first_character(index, info_str);
 	if (start_index == -1)
@@ -58,16 +62,18 @@ std::string get_first_word(int & index, const std::string & info_str) {
 	return info_str.substr(start_index, index - start_index);
 }
 
+//支出收入存储&转换类
 class Income_Expenses_Info {
 	std::vector<std::string> income_str = { "生活费","奖学金","稿费" };
 	std::vector<std::string> expense_str = { "学习用品","生活用品","游戏" };
 public:
-	bool income_expenses;
-	int num;
-	Time time;
-	double money;
-	std::string info;
-
+	bool income_expenses; // 支出还是收入
+	int num; // 支出或收入的第num类
+	Time time; //时间
+	double money; //金钱
+	std::string info; //备注
+	
+	// 字符串转换为有用信息
 	void operator()(const std::string & info_str) {
 		int index = 0;
 		auto str = get_first_word(index, info_str);
@@ -76,9 +82,9 @@ public:
 		{
 			str_list.emplace_back(str);
 			str = get_first_word(index, info_str);
-		}
+		}//字符串分割
 
-		std::stringstream stream;
+		std::stringstream stream;//类型转换专用类
 
 		income_expenses = (str_list[0][0] == 'a');
 
@@ -108,6 +114,7 @@ public:
 		info = str_list[3];
 	}
 
+	// 支持打印的友元函数
 	friend std::ostream &operator<<(std::ostream &cout, Income_Expenses_Info &info) {
 		cout << (info.income_expenses ? "收入" : "支出") << ' ';
 		cout << (info.income_expenses ? info.income_str[info.num - 1] : info.expense_str[info.num - 1]) << ' ';
@@ -117,6 +124,7 @@ public:
 		return cout;
 	}
 
+	// 支持文件保存的友元函数
 	friend std::ofstream &operator<<(std::ofstream &cout, Income_Expenses_Info &info) {
 		cout << (info.income_expenses ? 'a' : 'b');
 		cout << info.num << ' ';
@@ -127,8 +135,10 @@ public:
 	}
 };
 
+// 提示信息专用类
 class Recorder {
 public:
+	// 打印提示信息
 	void show_info() {
 		std::cout << "收入类:";
 		int i = 0;
@@ -149,6 +159,7 @@ private:
 	std::vector<std::string> expenses = { "学习用品","生活用品","游戏" };
 };
 
+// 将内存数据保存至文件，文件名为filename
 void info_list_to_file(const std::string & filename, std::vector<Income_Expenses_Info> & info_list) {
 	std::ofstream cout = std::ofstream(filename.data(), std::ios::out | std::ios::trunc);
 	for (auto info = info_list.begin(); info < info_list.end() - 1; ++info) {
@@ -158,6 +169,7 @@ void info_list_to_file(const std::string & filename, std::vector<Income_Expenses
 	cout.close();
 }
 
+// 将文件读取至内存，文件名为filename
 void file_to_info_list(const std::string & filename, std::vector<Income_Expenses_Info> & info_list) {
 	bool convert = false;
 	std::ifstream Reader(filename.data());
@@ -176,6 +188,7 @@ void file_to_info_list(const std::string & filename, std::vector<Income_Expenses
 	Reader.close();
 }
 
+// 信息统计类
 class Info_Statistics {
 	bool income_expenses;
 	int num;
@@ -203,6 +216,7 @@ public:
 		}
 	}
 
+	//  打印历史收支明细
 	void show_history() {
 		if(is_time)
 			std::cout << time.year << " 年 " << time.month << " 月收支类别数据的明细\n";
@@ -212,6 +226,7 @@ public:
 		}
 	}
 
+	//  获取符合时间段的收支明细
 	void get_from_time(const std::string & input, std::vector<Income_Expenses_Info> info_list) {
 		is_time = true;
 		std::vector<std::string> str_vector;
@@ -233,10 +248,11 @@ public:
 		{
 			expense[i] = 0;
 		}
-		for (const auto & info : info_list) {
+		
+		for (const auto & info : info_list) { // 穷举全部的信息
 			if (info.time.year == time.year &&
-				info.time.month == time.month) {
-				info_list_statistics.emplace_back(info);
+				info.time.month == time.month) { // 对比年月
+				info_list_statistics.emplace_back(info); // 存储有用信息
 				if (info.income_expenses) {
 					if (income.size() < info.num) {
 						income.emplace_back(info.money);
@@ -257,6 +273,7 @@ public:
 		}
 	}
 
+	//  获取符合类别的收支明细
 	void get_from_category(const std::string & input, std::vector<Income_Expenses_Info> info_list) {
 		is_time = false;
 		int i = 0;
@@ -311,6 +328,7 @@ public:
 		}
 	}
 
+	// 统计信息打印有元函数
 	friend std::ostream &operator<<(std::ostream &cout, Info_Statistics &info) {
 		if(info.is_time)
 			cout << info.time.year << " 年 " << info.time.month << " 月收支类别数据的汇总\n";
@@ -361,6 +379,8 @@ int main()
 
 	//查询信息
 	Info_Statistics info_statistics;
+
+	//查询信息按时间
 	std::cout << "请输入对收支类别数据进行汇总的月份 : ";
 	while (getline(std::cin, input)) {
 		if (input.size() == 0)
@@ -377,6 +397,7 @@ int main()
 		std::cin.ignore();
 	}
 
+	//查询信息按类别
 	std::cout << "请输入需要查询的类别 : ";
 	while (getline(std::cin, input)) {
 		if (input.size() == 0)
